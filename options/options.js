c8e5d8594db44058704456054c9e56752db31064
Crowdbotics <team@crowdbotics.com>
2023-02-03 01:26:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yard-tracker-38674",
+  url: "https://yard-tracker-38674.botics.co",
+  api: "https://yard-tracker-38674.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
